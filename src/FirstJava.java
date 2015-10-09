24f3961843f9981c54b8c6a0a23d5a5a4a4b5ff4
Hafsa <weirdsuzan@gmail.com>
2015-10-09 16:49:32 +0100

    Hi, I just added new files into the project

---
+++ src/FirstJava.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Hafsa on 09/10/2015.
+ */
+public class FirstJava {
+    public static void main(String [] args) {
+        //declare variables
+        int a, b, c;
+
+        //assign values to variable
+        a = 2;
+        b = 3;
+        c = b;
+
+        int x = (a + b + c);
+
+        System.out.println(x);
+
+    }
+}
