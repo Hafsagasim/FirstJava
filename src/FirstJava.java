059e2160ec9b0724b031e7d74c110d69b91e6766
Hafsa <weirdsuzan@gmail.com>
2015-10-11 23:03:28 +0100

    Ijust added few lines

--- src/FirstJava.java
+++ src/FirstJava.java
@@ -1,19 +1,59 @@
+
 /**
- * Created by Hafsa on 09/10/2015.
+ * Created by {Hafsa} on {09/10/2015.}
  */
-public class FirstJava {
+class FirstJava {
     public static void main(String [] args) {
+
+        System.out.println("Hello, People.");
+        System.out.println("Welcome to Java.");
+
+
+        System.out.println("Let's demonstrate a simple calculation.");
+
+        int answer;
+        answer = 2+2 ;
+        System.out.println("2 plus 2 is " + answer );
+
+
+
+
         //declare variables
-        int a, b, c;
+       // int a, b, c;
 
         //assign values to variable
-        a = 2;
-        b = 3;
-        c = b;
+        //a = 2;
+        //b = 3;
+        //c = b;
 
-        int x = (a + b + c);
+        //int x = (a + b + c);
 
-        System.out.println(x);
+        //System.out.println(x);
 
-    }
-}
+        char a, b;
+        a = 'A';
+        System.out.println(a);
+        b = 'B';
+        System.out.println(b);
+        a = b;
+        System.out.println(a);
+
+
+
+
+
+
+
+
+
+            }
+        }
+
+
+
+
+
+
+
+
+
